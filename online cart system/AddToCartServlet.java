4bd058e1c69c4ffcfc3bf7c157b1ffc7172244a6
Pratik Roy <72687129+pratikroy107@users.noreply.github.com>
2023-07-19 00:20:06 +0530

    Add files via upload

---
+++ online cart system/AddToCartServlet.java
@@ -0,0 +1,68 @@
+package cn.techtutorial.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import cn.techtutorial.model.*;
+
+/**
+ * @author Arijit
+ *
+ */
+@WebServlet("/add-to-cart")
+public class AddToCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		try (PrintWriter out = response.getWriter()) {
+//        	out.print("add to cart servlet");
+
+			ArrayList<Cart> cartList = new ArrayList<>();
+			int id = Integer.parseInt(request.getParameter("id"));
+			User auth = (User) request.getSession().getAttribute("auth");
+
+			if (auth != null) {
+				System.out.println("HII");
+				Cart cm = new Cart();
+				cm.setId(id);
+				cm.setQuantity(1);
+				HttpSession session = request.getSession();
+				ArrayList<Cart> cart_list = (ArrayList<Cart>) session.getAttribute("cart-list");
+				if (cart_list == null) {
+					cartList.add(cm);
+					session.setAttribute("cart-list", cartList);
+					response.sendRedirect("index.jsp");
+				} else {
+					cartList = cart_list;
+
+					boolean exist = false;
+					for (Cart c : cart_list) {
+						if (c.getId() == id) {
+							exist = true;
+							response.sendRedirect("cart.jsp");
+						}
+					}
+
+					if (!exist) {
+						cartList.add(cm);
+						response.sendRedirect("index.jsp");
+					}
+				}
+			} else {
+				response.sendRedirect("login.jsp");
+			}
+		}
+	}
+
+}
